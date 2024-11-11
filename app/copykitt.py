1a78a09d926b075063a2a8b13e7cf9d5959c1212
Julio Marodin <julio.marodin@gmail.com>
2024-11-11 14:25:08 -0300

    projeto finalizado

--- app/copykitt.py
+++ app/copykitt.py
@@ -1,6 +1,8 @@
 import openai
 import os
 import argparse
+import re
+from typing import List
 
 MAX_INPUT_LENGTH = 32
 
@@ -12,18 +14,52 @@ def main():
 
     print(f"User input: {user_input}")
     if validate_length(user_input):
-        branding_result = generate_branding_snippet(user_input)
-        print(branding_result)
+        generate_branding_snippet(user_input)
+        generate_keywords(user_input)
     else:
-        raise ValueError(f"Input length is too long. Must be under {MAX_INPUT_LENGTH}. Submitted input is: {user_input}")
+        raise ValueError(
+            f"Input length is too long. Must be under {MAX_INPUT_LENGTH}. Submitted input is {user_input}"
+        )
 
 def validate_length(prompt: str) -> bool:
     return len(prompt) <= MAX_INPUT_LENGTH
+
+def generate_keywords(prompt: str) -> List[str]:
+    client = openai.OpenAI()
+    # Load your API key from an environment variable or secret management service
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    enriched_prompt = f"Generate related branding keywords for {prompt}: "
+    print(enriched_prompt)
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "user",
+                "content": enriched_prompt
+            }
+        ]
+    )
+
+    # Extract output text.
+    keywords_text: str = response.choices[0].message.content
+    print("keywords:", keywords_text)
+
+    # Strip whitespace.
+    keywords_text = keywords_text.strip()
+    keywords_array = re.split(",|\n|;|-", keywords_text)
+    keywords_array = [k.lower().strip() for k in keywords_array]
+    keywords_array = [k for k in keywords_array if len(k) > 0]
+
+    print(f"Keywords: {keywords_array}")
+    return keywords_array
+
 
 def generate_branding_snippet(prompt:str) -> str:
     client = openai.OpenAI()
 
     client.api_key = os.getenv("OPENAI_API_KEY")
+    print("key:", os.getenv("OPENAI_API_KEY"))
 
     enriched_prompt = f"Generate upbeat branding snippet with hashtags for {prompt}: "
     print(enriched_prompt)
